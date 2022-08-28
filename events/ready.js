413078e45d6c2668e2a2715ab6a6146871317432
Mmmmmaaaappppxoooorrr <104400744+Mmmmmaaaappppxoooorrr@users.noreply.github.com>
2022-08-28 23:14:13 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,6 @@ const { Database } = require("quickmongo");
 const db = new Database(config.database);
    
    module.exports = async (client) => {
-   client.user.setPresence({ activity: { name: `+help`, type: "PLAYING" }, status: "online" });
+   client.user.setPresence({ activity: { name: `>help | Verifay Soon ^^`, type: "WATCHING" }, status: "online" });
       
    }
