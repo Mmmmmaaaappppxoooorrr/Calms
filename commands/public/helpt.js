b9de0b849c2db3f052a9f26ff49148047cd7e685
Mmmmmaaaappppxoooorrr <104400744+Mmmmmaaaappppxoooorrr@users.noreply.github.com>
2022-09-01 18:53:53 +0300

    Create helpt.js

---
+++ commands/public/helpt.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js")
+const { MessageEmbed } = require("discord.js")
+const { MessageButton } = require("discord-buttons")
+const { color } = require("../../config.json");
+const { lineReply } = require("discord-reply");
+
+module.exports = {
+  name: "helpyt",
+  aliases: [".", "invite", "support", "link"],
+  description: "links",
+  usage: "links",
+  run: async (client, message, args) => {
+  
+      let invite = new MessageButton().setStyle('url').setLabel('Invite Me').setURL('https://discord.com/api/oauth2/authorize?client_id=914082317344059402&permissions=8&scope=bot')
+      let support = new MessageButton().setStyle('url').setLabel('Soon').setURL('https://discord.gg/')
+     
+      const allbuttons = [invite, support]
+
+    let mybuttonsmsg = await message.channel.send({
+        embed: new MessageEmbed()
+          .setColor(color)
+          .setDescription('**Click on The buttons below to redirect**')
+          .addField("Need Help?", 'Join To Our **Support Server**!')
+          .addField("Use Me?", '**Invite me** now by [Click Here](https://discord.com/api/oauth2/authorize?client_id=914082317344059402&permissions=8&scope=bot) or click on the **button**'),
+        buttons: allbuttons
+      });
+  }
+}
